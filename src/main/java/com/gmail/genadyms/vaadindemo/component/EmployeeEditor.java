19cb8ff2e9665775058429422a9c43df06543217
genadyms <genadyms@gmail.com>
2020-12-03 00:56:48 +0300

    Added form for employee

---
+++ src/main/java/com/gmail/genadyms/vaadindemo/component/EmployeeEditor.java
@@ -0,0 +1,87 @@
+package com.gmail.genadyms.vaadindemo.component;
+
+import com.gmail.genadyms.vaadindemo.entity.Employee;
+import com.gmail.genadyms.vaadindemo.repo.EmployeeRepo;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.KeyNotifier;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.spring.annotation.SpringComponent;
+import com.vaadin.flow.spring.annotation.UIScope;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringComponent
+@UIScope
+public class EmployeeEditor extends VerticalLayout implements KeyNotifier {
+    private final EmployeeRepo employeeRepo;
+    private Employee employee;
+
+    private TextField firstName = new TextField("First Name");
+    private TextField lastName = new TextField("Last Name");
+    private TextField patronymic = new TextField("Patronymic");
+
+    private Button save = new Button("save", VaadinIcon.CHECK.create());
+    private Button cancel = new Button("cancel");
+    private Button delete = new Button("delete", VaadinIcon.TRASH.create());
+    private HorizontalLayout actions = new HorizontalLayout(save, cancel, delete);
+
+    private Binder<Employee> binder = new Binder<>(Employee.class);
+    @Setter
+    private ChangeHandler changeHandler;
+
+    public interface ChangeHandler {
+        void onChange();
+    }
+
+    @Autowired
+    public EmployeeEditor(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+        add(firstName, lastName, patronymic, actions);
+        binder.bindInstanceFields(this);
+        // Configure and style components
+        setSpacing(true);
+
+        save.getElement().getThemeList().add("primary");
+        delete.getElement().getThemeList().add("error");
+
+        addKeyPressListener(Key.ENTER, e -> save());
+
+        // wire action buttons to save, delete and reset
+        save.addClickListener(e -> save());
+        delete.addClickListener(e -> delete());
+        cancel.addClickListener(e -> edit(employee));
+        setVisible(false);
+    }
+
+    public void edit(Employee inputEmployee) {
+        if (inputEmployee == null) {
+            setVisible(false);
+            return;
+        }
+
+        if (inputEmployee.getId() != null) {
+            employee = employeeRepo.findById(inputEmployee.getId()).orElse(inputEmployee);
+        } else {
+            employee = inputEmployee;
+        }
+
+        binder.setBean(employee);
+        setVisible(true);
+        lastName.focus();
+    }
+
+    void delete() {
+        employeeRepo.delete(employee);
+        changeHandler.onChange();
+    }
+
+    void save() {
+        employeeRepo.save(employee);
+        changeHandler.onChange();
+    }
+}
--- src/main/java/com/gmail/genadyms/vaadindemo/view/MainView.java
+++ src/main/java/com/gmail/genadyms/vaadindemo/view/MainView.java
@@ -1,9 +1,14 @@
 package com.gmail.genadyms.vaadindemo.view;
 
+import com.gmail.genadyms.vaadindemo.component.EmployeeEditor;
 import com.gmail.genadyms.vaadindemo.entity.Employee;
 import com.gmail.genadyms.vaadindemo.repo.EmployeeRepo;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,13 +16,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class MainView extends VerticalLayout {
 
     private final EmployeeRepo employeeRepo;
-
     private Grid<Employee> grid = new Grid<>(Employee.class);
 
+    private final TextField filter = new TextField("", "Type to filter");
+    private final Button addEmployee = new Button("Add new");
+    private final HorizontalLayout toolbar = new HorizontalLayout(filter, addEmployee);
+
+    private final EmployeeEditor editor;
+
     @Autowired
-    public MainView(EmployeeRepo employeeRepo) {
+    public MainView(EmployeeRepo employeeRepo, EmployeeEditor editor) {
         this.employeeRepo = employeeRepo;
-        add(grid);
-        grid.setItems(employeeRepo.findAll());
+        this.editor = editor;
+        add(toolbar, grid, editor);
+
+
+        // Replace listing with filtered content when user changes filter
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> showEmployee(e.getValue()));
+
+        // Connect selected Customer to editor or hide if none is selected
+        grid.asSingleSelect().addValueChangeListener(e -> {
+            editor.edit(e.getValue());
+        });
+
+        // Instantiate and edit new Customer the new button is clicked
+        addEmployee.addClickListener(e -> editor.edit(new Employee()));
+
+        // Listen changes made by the editor, refresh data from backend
+        editor.setChangeHandler(() -> {
+            editor.setVisible(false);
+            showEmployee(filter.getValue());
+        });
+
+        showEmployee("");
+    }
+
+    private void showEmployee(String name) {
+        if (name.isEmpty()) {
+            grid.setItems(employeeRepo.findAll());
+        } else {
+            grid.setItems(employeeRepo.findByName(name));
+        }
     }
 }
