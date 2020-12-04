b320c6b9992d087872ff9bdeab3c05bb79c00967
genadyms <genadyms@gmail.com>
2020-12-04 16:51:54 +0300

    Added the grid showing

--- src/main/java/com/gmail/genadyms/vaadindemo/ui/view/MainView.java
+++ src/main/java/com/gmail/genadyms/vaadindemo/ui/view/MainView.java
@@ -1,16 +1,34 @@
 package com.gmail.genadyms.vaadindemo.ui.view;
 
+import com.gmail.genadyms.vaadindemo.backend.entity.Contact;
+import com.gmail.genadyms.vaadindemo.backend.service.ContactService;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
 
-@Route
+@Route("")
 public class MainView extends VerticalLayout {
+    private ContactService contactService;
 
+    private Grid<Contact> grid = new Grid<>(Contact.class);
 
-    @Autowired
-    public MainView() {
+    public MainView(ContactService contactService) {
+        this.contactService = contactService;
+        addClassName("list-view");
+        setSizeFull();
+        configureGrid();
+        add(grid);
 
+        updateList();
     }
 
+    private void configureGrid() {
+        grid.addClassName("contact-grid");
+        grid.setSizeFull();
+        grid.setColumns("firstName", "lastName", "email", "status");
+    }
+
+    private void updateList() {
+        grid.setItems(contactService.findAll());
+    }
 }
